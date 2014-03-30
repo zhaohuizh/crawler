9b0d9e77e77249a0374171cb71d06f7e5e56cdc2
zhaohuizh <bravezhaohui@gmail.com>
2014-03-30 16:34:37 -0400

    add start.sh and crawler.py

---
+++ crawler.py
@@ -0,0 +1,24 @@
+import json
+from glassdoor import get
+
+dest_path = 'company.json'
+json_data = open('companies/data.json')
+data = json.load(json_data)
+com_list = []
+for value in data:
+  com_list.append(value['name'])
+json_data.close()
+#remove duplicates
+com_list = list(set(com_list))
+print 'Len: ', len(com_list)
+
+obj = open(dest_path, 'w')
+obj.write('[')
+for one in com_list:
+  info = get(one)
+  obj.write('{ name:' + one +',')
+  obj.write(json.dumps(info))
+  obj.write('},')
+  obj.flush()
+obj.write(']')
+obj.close()
